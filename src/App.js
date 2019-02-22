c9a68176d0ccd875120cfab68872bea0bcc45e1a
Your Name Here <leeye9403@hanmail.net>
2019-02-22 11:16:28 +0900

    고객컴포넌트 구조화 & map함수 이용하여 배열간단히 하기

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
-  'name' : '이예은',
+const customers = [
+  {
+  'id' :1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name' : '아무개',
+  'birthday' : '970330',
+  'gender' : '여자',
+  'job' : '대학생'
+},
+{
+  'id' :2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '940330',
+  'gender' : '여자',
+  'job' : '대학생'
+},
+{
+  'id' :3,
+  'image' :'https://placeimg.com/64/64/3',
+  'name' : '어저구',
   'birthday' : '940330',
   'gender' : '여자',
   'job' : '대학생'
 }
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return(
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                />
+            )
+          })
+        }
+      </div>
     );
   }
 }
